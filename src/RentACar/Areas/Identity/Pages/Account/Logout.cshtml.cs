4e94a119e24a7da5385e7748f9925eebc013cefd
Stoyan Stoyanov <stoyan.stoyanov123@gmail.com>
2021-08-14 13:37:25 +0300

    Adding base structure of Layout and Login Partial; Adding a migration; Fit Login, Logout and Register logic and views

--- src/RentACar/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/RentACar/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,25 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using RentACar.Data.Models;
 
 namespace RentACar.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+        private readonly SignInManager<User> _signInManager;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager)
         {
             _signInManager = signInManager;
-            _logger = logger;
         }
 
         public void OnGet()
@@ -29,7 +24,6 @@ namespace RentACar.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
